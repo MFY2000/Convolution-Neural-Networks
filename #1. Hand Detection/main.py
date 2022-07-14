65b07e3decfed5489c31e0d365add7909f4dbd37
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-07-15 00:15:40 +0500

    nwe

--- #1. Hand Detection/main.py
+++ #1. Hand Detection/main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import cv2
+#import mediapipe as mp
+import time
+
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+    print("Hello")
