41ea169ee1a6cd9462b8fdf2d3dbb41f0b09cd89
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-07-16 23:04:45 +0500

    MFY auto commit at 16/07/2022 23:04:43

--- #1. Hand Detection/main.py
+++ #1. Hand Detection/main.py
@@ -16,15 +16,20 @@ import cv2
 import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
-detector = mp.solutions.hands.Hands()
+
+detector = mp.solutions.hands
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    result =  detector.process(img)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result =  detector.Hands().process(imgRGB)
 
-    if result.multi_hand_
-    cv2.imshow("AI", img)
+    if result.multi_hand_landmarks:
+        for handlms in result.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handlms, detector.HAND_CONNECTIONS)
+
+    cv2.imshow("AI", img) 
     cv2.waitKey(1)
 
