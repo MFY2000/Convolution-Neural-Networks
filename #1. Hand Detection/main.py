1ed1f30083ca895c8a175d3e5b6d18c90a688d0e
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-07-16 10:20:25 +0500

    MFY auto commit at 16/07/2022 10:18:33

--- #1. Hand Detection/main.py
+++ #1. Hand Detection/main.py
@@ -1,13 +1,30 @@
+# import cv2
+# import time
+#
+#
+# cap = cv2.VideoCapture(0)
+#
+# while True:
+#     imgnee = cap.read()
+#     # imgnee = cv2.imread("1.jpg")
+#     cv2.imshow("Image", imgnee)
+#     cv2.waitKey(0)
+#     print("Hello")
+
+
 import cv2
-#import mediapipe as mp
-import time
+import mediapipe as mp
 
-
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+detector = mp.solutions.hands.Hands()
 
 while True:
-    img = cap.read()
-    cv2.imshow("Image", img)
+    success, img = cap.read()
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result =  detector.process(img)
+
+    if result.multi_hand_
+    cv2.imshow("AI", img)
     cv2.waitKey(1)
 
-    print("Hello")
